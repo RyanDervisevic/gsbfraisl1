67aa745978074ed06760ae7ec3fa53bfd2ce9d74
NicolasJeannette <44873578+NicolasJeannette@users.noreply.github.com>
2018-12-20 08:19:06 +0100

    Add files via upload

---
+++ app/Http/Controllers/NewVisiteurController.php
@@ -0,0 +1,23 @@
+<?php
+
+ 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use App\metier\GsbFrais;
+
+
+class NewVisiteurController extends Controller
+{
+    public function NewVisiteur()
+    {
+        return view('newuser');
+        
+        
+    }
+    
+    
+    
+}
+
