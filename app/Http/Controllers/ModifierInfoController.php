18baddd110e7fab10c3ce07f980f9d33d6e07392
NicolasJeannette <44873578+NicolasJeannette@users.noreply.github.com>
2018-12-20 16:06:51 +0100

    Update ModifierInfoController.php

--- app/Http/Controllers/ModifierInfoController.php
+++ app/Http/Controllers/ModifierInfoController.php
@@ -12,24 +12,33 @@ class ModifierInfoController extends Controller
 {
     public function changeInfo(Request $request)
     {
-        $login = Session::get('login');
-        $cp = Session::get('cp');
-        $ville = Session::get('ville');
-        $mail = Session::get('email');
-        $tel = Session::get('telephone');
-        $uneInfo = new GsbFrais();
-        $uneModifInfo = $uneInfo->setdonnee($login,$cp,$ville,$mail,$tel);
-       // print_r($unFraisHorsForfait);
         
-            
-            
+        $id = Session::get('id');
+        $adresse = $request->input('adresse');
+        $cp = $request->input('cp');
+        $ville = $request->input('ville');
+        $mail = $request->input('email');
+        $telephone = $request->input('telephone');
         
+        $uneModifa = new GsbFrais();
+        $uneModifa->setdonnee($id,$adresse,$cp,$ville,$mail,$telephone);
+   
         
         return view('changeinfo');
-        
+ 
+    }
+    public function ancienInfo()
+    {
+        $id = Session::get('id');
+        Session::put('adresse');
+        Session::put('cp');
+        Session::put('ville');
+        Session::put('telephone');
+        Session::put('email');
+        $uneModifan = new GsbFrais();
+        $uneModifan->getdonnee($id);
         
     }
-    
     
     
 }
