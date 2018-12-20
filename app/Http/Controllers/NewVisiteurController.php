4a0ab6a4cd7b7b5494ed95a426e4ad3ce5992f71
NicolasJeannette <44873578+NicolasJeannette@users.noreply.github.com>
2018-12-20 16:06:01 +0100

    Update NewVisiteurController.php

--- app/Http/Controllers/NewVisiteurController.php
+++ app/Http/Controllers/NewVisiteurController.php
@@ -10,9 +10,19 @@ use App\metier\GsbFrais;
 
 class NewVisiteurController extends Controller
 {
-    public function NewVisiteur()
+    public function NewVisiteur(request $request)
     {
-        
+        $idVisiteur = $request->input('id');
+        $nom = $request->input('nom');
+        $prenom = $request->input('prenom');
+        $adresse = $request->input('adresse');
+        $cp = $request->input('cp');
+        $ville = $request->input('ville');
+        $date = $request->input('dateEmbauche');
+        $mail = $request->input('email');
+        $telephone = $request->input('telephone');
+        $unNVU = new GsbFrais();
+        $unNVU->creervisiteur($idVisiteur,$nom, $prenom, $adresse, $cp, $ville, $date, $mail, $telephone);
         
         
         return view('newuser');
@@ -23,4 +33,5 @@ class NewVisiteurController extends Controller
     
     
 }
+
 
