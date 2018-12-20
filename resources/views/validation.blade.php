d84f06ff41a167ab2293ac8d8accb00887951c30
maxime-gros <maxime.gros3957@gmail.com>
2018-12-20 15:24:51 +0100

    passage sous git

--- resources/views/validation.blade.php
+++ resources/views/validation.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('content')
 {!! Form::open(['url' => 'validation']) !!} 
-<a> La fiche de frais a bien été validée</a>
+<p> La fiche de frais a bien été validée</p>
 <a href="{{ url('/validerFRAIS')}}" ><button type="button" class="btn btn-default btn-primary" >Retour</button></a>
 {!! Form::close() !!}
 @stop
